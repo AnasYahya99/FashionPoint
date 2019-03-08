7f5ac80483b2d925e9557718ab4c47b90b0f4d3f
Anas Yahya <2446628y@student.gla.ac.uk>
2019-03-08 15:56:13 +0200

    media

--- fashionpoint/urls.py
+++ fashionpoint/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.conf.urls import include 
 from django.contrib import admin
-from fashionpointapp import views	
+from fashionpointapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^fashionpoint/', include('fashionpointapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
