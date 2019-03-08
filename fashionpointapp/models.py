0e1aa48ca0fa5e77e5cb5b0171cc1080067c441c
Anas Yahya <2446628y@student.gla.ac.uk>
2019-03-08 16:25:57 +0200

    models1

--- fashionpointapp/models.py
+++ fashionpointapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	def __str__(self):
+		return self.name
+class Post(models.Model):
+	category = models.ManyToManyField(Category)
+	rating = models.FloatField(default=0)
+	date = models.DateField(("Date"), default=date.today)
+	def __str__(self): 
+		return self.id
+
