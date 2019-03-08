68b5992dfbc756546c0f9454a1a7e4207e324f33
Anas Yahya <2446628y@student.gla.ac.uk>
2019-03-08 18:51:51 +0200

    models

--- fashionpointapp/admin.py
+++ fashionpointapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from fashionpointapp.models import Category, Post, Poll, PostComment, PollComment, Rating, Vote, UserProfile
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Poll)
+admin.site.register(PostComment)
+admin.site.register(PollComment)
+admin.site.register(Rating)
+admin.site.register(Vote)
--- fashionpointapp/models.py
+++ fashionpointapp/models.py
@@ -1,14 +1,88 @@
 from django.db import models
 from datetime import date
+from datetime import datetime
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	dateOfBirth = models.DateField();
+	class Meta: 
+		verbose_name_plural = 'Users'
+	def __str__(self):
+		return self.user.username
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	class Meta: 
+		verbose_name_plural = 'Categories'
 	def __str__(self):
 		return self.name
 class Post(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 	category = models.ManyToManyField(Category)
-	rating = models.FloatField(default=0)
-	date = models.DateField(("Date"), default=date.today)
-	def __str__(self): 
-		return self.id
+	description = models.CharField(max_length=128,null=True,blank=True)
+	avgRating = models.FloatField(default=0)
+	date = models.DateTimeField(auto_now=True)
+	photo = models.ImageField(upload_to="posts/")
+	class Meta: 
+		verbose_name_plural = 'Posts'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
+class Poll(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	category = models.ManyToManyField(Category)
+	description = models.CharField(max_length=128,null=True,blank=True)
+	date = models.DateTimeField(auto_now=True)
+	picture1 = models.ImageField(upload_to="polls/")
+	picture2 = models.ImageField(upload_to="polls/")
+	picture1Clicks = models.PositiveIntegerField(default=0)
+	picture2Clicks = models.PositiveIntegerField(default=0)
+	class Meta: 
+		verbose_name_plural = 'Polls'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
 
+class PostComment(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	comment = models.CharField(max_length=256,null=True,blank=True)
+	date = models.DateTimeField(auto_now=True)
+	numOfLikes = models.PositiveIntegerField(default=0)
+	numOfDislikes = models.PositiveIntegerField(default=0)
+	class Meta: 
+		verbose_name_plural = 'Post Comments'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
+class PollComment(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+	comment = models.CharField(max_length=256,null=True,blank=True)
+	date = models.DateTimeField(auto_now=True)
+	numOfLikes = models.PositiveIntegerField(default=0)
+	numOfDislikes = models.PositiveIntegerField(default=0)
+	class Meta: 
+		verbose_name_plural = 'Poll Comments'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
+class Rating(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	rating = models.PositiveIntegerField(default=0)
+	class Meta: 
+		verbose_name_plural = 'Ratings'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
+class Vote(models.Model):
+	userPofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+	choice = models.BooleanField();
+	class Meta: 
+		verbose_name_plural = 'Votes'
+	def __str__(self):
+		stringID = str(self.id)
+		return stringID
