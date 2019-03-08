9a0f39cafb865e69b9ca36951ef9953511f82fb0
Anas Yahya <2446628y@student.gla.ac.uk>
2019-03-08 15:36:44 +0200

    fix

--- fashionpoint/urls.py
+++ fashionpoint/urls.py
@@ -3,12 +3,6 @@ from django.conf.urls import include
 from django.contrib import admin
 from fashionpointapp import views	
 urlpatterns = [
-<<<<<<< HEAD
     url(r'^admin/', admin.site.urls),
     url(r'^fashionpoint/', include('fashionpointapp.urls')),
-=======
-	url(r'^$', views.index, name='index'),
-	url(r'^admin/', admin.site.urls),
-	url(r'^fashionpoint/', include('fashionpointapp.urls')),
->>>>>>> 9248fba15c6c2356a061512c8f20c2dd6f786964
 ]
