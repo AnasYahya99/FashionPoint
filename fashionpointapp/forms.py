aa431a088a0f9f258115b64f04f71426d52bab1c
Noura Abulaban <2446632A@student.gla.ac.uk>
2019-03-11 08:21:42 +0200

    login

--- fashionpointapp/forms.py
+++ fashionpointapp/forms.py
@@ -1,16 +1,13 @@
 from django import forms
-from fashionpointapp.models import Post,Category
+from fashionpointapp.models import Post,Category,UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
+class RegisterForm(UserCreationForm):
+	email = forms.EmailField()
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
     description = forms.CharField(max_length=256)
@@ -18,7 +15,6 @@ class PostForm(forms.ModelForm):
     photo = forms.ImageField()
     date = forms.DateTimeField(widget=forms.HiddenInput())
     category = forms.MultipleChoiceField(choices=Category.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
-    #userPofile = forms.HiddenInput(intial = user)
     class Meta:
         model = Post
         fields = ('description','photo','category')
--- fashionpointapp/views.py
+++ fashionpointapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from fashionpointapp.forms import PostForm
-from .forms import UserRegisterForm
+from .forms import RegisterForm
 
 def index(request):
  return render(request, 'fashionpointapp/index.html',)
@@ -29,17 +29,17 @@ def about_us(request):
 	return render(request, 'fashionpointapp/about.html',)
 def sitemap(request):
 	return render(request, 'fashionpointapp/sitemap.html',)
-	
+
 def signup(request):
 		if request.method == 'POST':
-			form = UserRegisterForm(request.POST)
-			if form.is_valid():
+			form = RegisterForm(request.POST)
+			if form.is_valid() :
 				form.save()
 				username = form.cleaned_data.get('username')
-				messages.success(request, f'Account created for {username}!')
+				messages.success(request, f'Successfly created the account!')
 				return redirect(request, 'fashionpointapp/index.html',)
 		else:
-			form = UserRegisterForm()
+			form = RegisterForm()
 		return render(request, 'fashionpointapp/signup.html',{'form': form}) 
 @login_required
 def PostaPost(request):
@@ -53,15 +53,46 @@ def PostaPost(request):
     else:
         print(form.errors)
     return render(request, 'fashionpointapp/PostaPost.html', {'form': form})
-
-
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("Account is disabled.")
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'Fashionpointapp/login.html', {})
+@login_required
+def restricted(request):
+	return render(request,'Fashionpointapp/restricted.html',{})
 @login_required
 def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('index'))
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+											   'last_visit',
+											   str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+							'%Y-%m-%d %H:%M:%S')
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		request.session['last_visit'] = last_visit_cookie
+	request.session['visits'] = visits
 
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
 
-def user_login(request):
-    user = authenticate(username='Anas', password='Anas1234')
-    login(request, user)
-    return HttpResponseRedirect(reverse('index'))
